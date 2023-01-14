50cbad8de071941d610a7ed86290540109c78095
cbk914 <cbk914@riseup.net>
2023-01-14 20:34:59 +0100

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # OpenVuln-cli
-Interface to the OpenVuln API
+
+Interface to the OpenVuln API.
+
+**In order to use this script, you will need to have a valid Cisco PSIRT OpenVuln API username and API key.**
+
+To use the Cisco PSIRT OpenVuln API console interface in Python, follow these steps:
+
+1. Make sure you have a valid Cisco account and API key. You will need to sign up for an account if you do not have one.
+2. Install the necessary libraries by running "pip install requests" in your command line.
+3. Download the script and run it with Python.
+4. Use the -v or --vuln option to input the vulnerability ID that you want to look up. For example: "python openvuln-cli.py -v cisco-sa-20180829-asr1k"
+5. Use the -o or --output option to export the results to a .txt file. For example: "python openvuln-cli.py -v cisco-sa-20180829-asr1k -o results.txt"
+6. Use the -u or --user option to input your Cisco account username. For example: "python openvuln-cli.py -v cisco-sa-20180829-asr1k -o results.txt -u my_username"
+7. Use the -api or --api_key option to input your Cisco account API key. For example: "python openvuln-cli.py -v cisco-sa-20180829-asr1k -o results.txt -u my_username -api my_API_key"
+8. The script will use the provided credentials to authenticate with the Cisco PSIRT OpenVuln API and retrieve information about the specified vulnerability. The results will be printed to the console and, if specified, written to the specified .txt file.
+
+Note: Be sure to keep your Cisco account and API key private, and never share them with other
+
+Note: This script is using requests, argparse and json librares, please make sure you have them installed.
--- openvuln-cli.py
+++ openvuln-cli.py
@@ -5,31 +5,62 @@ import requests
 import json
 
 # Parse command line arguments
-parser = argparse.ArgumentParser(description='Retrieve information from Cisco PSIRT OpenVuln API')
-parser.add_argument('-v', '--vuln', required=True, help='Vulnerability ID')
-parser.add_argument('-o', '--output', help='Output file')
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--vuln", help="Vulnerability ID")
+parser.add_argument("-o", "--output", help="Output file")
+parser.add_argument("-u", "--username", required=True, help="Cisco PSIRT OpenVuln API username")
+parser.add_argument("-api", "--apikey", required=True,help="Cisco PSIRT OpenVuln API key")
+parser.add_argument("-s", "--search", help="Search term (name or CVE)")
 args = parser.parse_args()
 
-# Cisco PSIRT OpenVuln API endpoint and headers
-endpoint = 'https://api.cisco.com/security/advisories/'
-headers = {'Accept': 'application/json', 'Authorization': 'Basic YOUR_AUTH_TOKEN'}
+# Authenticate with Cisco PSIRT OpenVuln API
+auth_url = "https://api.cisco.com/security/advisories/psirt-openvuln/authenticate"
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+payload = {
+    "username": args.username,
+    "apikey": args.apikey
+}
+response = requests.post(auth_url, headers=headers, json=payload)
 
-# Send GET request to retrieve vulnerability information
-response = requests.get(endpoint + args.vuln, headers=headers)
+# Extract the authentication token from the response
+token = response.json()["token"]
+
+# Add the token to the headers for future requests
+headers["Authorization"] = "Bearer " + token
 
 # Check if the request was successful
 if response.status_code != 200:
     print('Error: Failed to retrieve vulnerability information')
     exit(1)
+    
+# Search for vulnerabilities by name or CVE
+if args.search:
+    search_url = "https://api.cisco.com/security/advisories/psirt-openvuln/vuln-search"
+    # endpoint = "https://api.cisco.com/security/advisories/psirt-openvuln-api/v1"
+    search_payload = {
+        "search": args.search
+    }
+    search_response = requests.post(search_url, headers=headers, json=search_payload)
+    print(search_response.json())    
 
 # Parse JSON response
 vuln_info = json.loads(response.text)
 
 # Print vulnerability information to console
-print('Vulnerability ID:', vuln_info['id'])
-print('CVSS Score:', vuln_info['cvss'])
-print('Title:', vuln_info['title'])
-print('Advisory:', vuln_info['advisory'])
+print("ID:", data['id'])
+print("Advisory ID:", data['advisoryId'])
+print("CVSS Base Score:", data['cvssBaseScore'])
+print("CVSS Temporal Score:", data['cvssTemporalScore'])
+print("CVSS Vector:", data['cvssVector'])
+print("CWE ID:", data['cweId'])
+print("Description:", data['description'])
+print("Publish Date:", data['publishDate'])
+print("Software Versions:", data['softwareVersions'])
+print("Threat Category:", data['threatCategory'])
+print("Title:", data['title'])
 
 # Export the vulnerability details to a file
 if args.output:
